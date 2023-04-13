dce22995168057ee1e141d4c1e52b6bae5937f43
Sveta <60633351+silmu@users.noreply.github.com>
2023-04-13 19:54:01 +0300

    Fix bug when a task is deleted

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,16 +1,11 @@
-import React from 'react';
-import List from '@mui/material/List';
-import {
-  DragDropContext,
-  Droppable,
-  Draggable,
-  DropResult,
-} from 'react-beautiful-dnd';
+import React from "react";
+import List from "@mui/material/List";
+import { DragDropContext, Droppable, Draggable, DropResult } from "react-beautiful-dnd";
 
-import { setAllTasks } from './features/todoList/ToDoListSlice';
-import { useAppDispatch } from './features/todoList/ToDoListSlice';
+import { setAllTasks } from "./features/todoList/ToDoListSlice";
+import { useAppDispatch } from "./features/todoList/ToDoListSlice";
 
-import TodoTask from './TodoTask';
+import TodoTask from "./TodoTask";
 
 const TodoList: React.FC<{
   list: { id: number; name: string; checked: boolean }[];
@@ -29,17 +24,14 @@ const TodoList: React.FC<{
     //If element is dropped outside of the droppable area
     if (!result.destination) return;
 
-    const reorderedTasks = reorder(
-      result.source.index,
-      result.destination.index
-    );
+    const reorderedTasks = reorder(result.source.index, result.destination.index);
     dispatch(setAllTasks(reorderedTasks));
     //Set localStorage
-    localStorage.setItem('tasks', JSON.stringify(reorderedTasks));
+    localStorage.setItem("tasks", JSON.stringify(reorderedTasks));
   };
 
   return (
-    <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
+    <List sx={{ width: "100%", bgcolor: "background.paper" }}>
       {/* {list.map(task => {
         return ( */}
       <DragDropContext onDragEnd={result => handleOnDragEnd(result)}>
@@ -47,21 +39,10 @@ const TodoList: React.FC<{
           {provided => (
             <div {...provided.droppableProps} ref={provided.innerRef}>
               {list.map((task, index) => (
-                <Draggable
-                  key={task.id}
-                  draggableId={task.id.toString()}
-                  index={index}
-                >
+                <Draggable key={task.id} draggableId={task.id.toString()} index={index}>
                   {provided => (
-                    <div
-                      ref={provided.innerRef}
-                      {...provided.draggableProps}
-                      {...provided.dragHandleProps}
-                    >
-                      <TodoTask
-                        task={task}
-                        labelId={`checkbox-list-label-${task?.id}`}
-                      />
+                    <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                      <TodoTask task={task} labelId={`checkbox-list-label-${task?.id}`} />
                     </div>
                   )}
                 </Draggable>
